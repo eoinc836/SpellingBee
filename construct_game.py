f8286e5612e14f834d82b39d98dcee7535ce418a
eoinc836 <eoinc836@gmail.com>
2026-08-05 19:15:25 +0100

    improve game state

--- construct_game.py
+++ construct_game.py
@@ -6,23 +6,30 @@ from signal import pause
 
 scrape_bee('https://www.nytimes.com/puzzles/spelling-bee')
 game_data_file_name = datetime.today().strftime('%Y-%m-%d') + '.json'
-global word
-word=""
 with open('data/'+game_data_file_name) as game_data:
     data = json.load(game_data)
     centerLetter = data['today']['centerLetter']
     outerLetters = data['today']['outerLetters']
 
 
-def addLetterToWord(letter):
-    word += letter
-    print(word)
+class GameState:
+    def __init__(self):
+        self.word = ""
+
+    def add_letter(self, letter):
+        self.word += letter
+        print(self.word)
+
+    def clear(self):
+        self.word = ""
+
+state = GameState()
 
 centerLetter_button = Button(27)
 outer_letter_1 = Button(22)
 
-centerLetter_button.when_pressed = lambda: addLetterToWord(centerLetter)
-outer_letter_1.when_pressed = lambda: addLetterToWord(outerLetters[0])
+centerLetter_button.when_pressed = lambda: state.add_letter(centerLetter)
+outer_letter_1.when_pressed = lambda: state.add_letter(outerLetters[0])
 
 pause()
 
