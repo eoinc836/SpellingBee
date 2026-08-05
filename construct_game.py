dcc119ff2bf797a5300935f10376221c5def7c9e
eoinc836 <eoinc836@gmail.com>
2026-08-05 21:06:38 +0100

    more game actions

--- construct_game.py
+++ construct_game.py
@@ -10,18 +10,31 @@ with open('data/'+game_data_file_name) as game_data:
     data = json.load(game_data)
     centerLetter = data['today']['centerLetter']
     outerLetters = data['today']['outerLetters']
-
+    answers = data['today']['answers']
+    pangrams = data['today']['pangram']
 
 class GameState:
     def __init__(self):
         self.word = ""
-
+        self.score = 0
     def add_letter(self, letter):
         self.word += letter
         print(self.word)
 
     def clear(self):
         self.word = ""
+
+    def guess(self):
+        if self.word in answers:
+            if len(self.word) == 4:
+                points = 1
+            elif self.word not in pangrams:
+                points = len(self.word) - 3
+            self.score += points
+            print("Correct. Your score is now", self.score)
+        else:
+            print("Not a valid word",self.word)
+
 
 state = GameState()
 
@@ -32,7 +45,8 @@ outer_letter_3 = Button(24)
 outer_letter_4 = Button(21)
 outer_letter_5 = Button(20)
 outer_letter_6 = Button(25)
-
+clear_button = Button(16)
+enter_button = Button(12)
 
 centerLetter_button.when_pressed = lambda: state.add_letter(centerLetter)
 outer_letter_1.when_pressed = lambda: state.add_letter(outerLetters[0])
@@ -41,6 +55,8 @@ outer_letter_3.when_pressed = lambda: state.add_letter(outerLetters[2])
 outer_letter_4.when_pressed = lambda: state.add_letter(outerLetters[3])
 outer_letter_5.when_pressed = lambda: state.add_letter(outerLetters[4])
 outer_letter_6.when_pressed = lambda: state.add_letter(outerLetters[5])
+clear_button.when_pressed = lambda: state.clear()
+enter_button.when_pressed = lambda: state.guess()
 
 pause()
 
